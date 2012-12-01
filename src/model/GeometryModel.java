574143a5179cdfd6b3ed78eca67fa2f444fcbf72
Bc. Petr Povala <povalpet@fit.cvut.cz>
2012-12-01 14:30:21 +0100

    observer

--- src/model/GeometryModel.java
+++ src/model/GeometryModel.java
@@ -1,18 +1,41 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class GeometryModel implements IModel {
-	private int id;
+import observer.Observable;
+import observer.Observer;
+
+public class GeometryModel implements IModel, Observable {
 	private Map<Integer,IGeometry> objectList;
+	private List<Observer> observerList;
 	
 	public GeometryModel() {
-		this.objectList = new HashMap<>();
-		this.id = 0;
+		this.objectList = new HashMap<Integer, IGeometry>();
+		this.observerList = new ArrayList<Observer>();
 	}
 	
 	public void addNewGeometry(IGeometry geometry) {
-		this.objectList.put(new Integer(this.id++), geometry);
+		this.objectList.put(new Integer(geometry.getId()), geometry);
+		this.notifyObservers();
+	}
+
+	@Override
+	public void attach(Observer observer) {
+		this.observerList.add(observer);
+	}
+
+	@Override
+	public void detach(Observer observer) {
+		// TODO Auto-generated method stub		
+	}
+
+	@Override
+	public void notifyObservers() {
+		for(Observer ob : observerList) {
+			ob.update();
+		}
 	}
 }
